f2f79d67414c8181dbfc35822abfe179bfcf46d5
David <dsem562@gmail.com>
2022-03-31 03:47:07 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+function superbowlWin(array) {    
+    const results = array.find(array => array.result === "W");
+    if(results){
+        return results.year;
+    }
+}
